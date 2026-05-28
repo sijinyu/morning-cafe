cf988192bf4574971d80e4bf2f2205d70e29ec5a
유시진/FE파트/NE <sijinyu@nhn.com>
2026-05-28 11:45:21 +0900

    fix: 카카오 JS SDK integrity 해시 업데이트
    카카오가 SDK 파일(2.7.4)을 갱신하여 기존 SHA-384 해시 불일치로
    SDK 로드가 브라우저에서 차단됨 → 지도/공유 등 전체 기능 불가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -100,7 +100,7 @@ export default function RootLayout({
           <>
             <Script
               src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
-              integrity="sha384-DKYJZ8NLiK8MN4/C5P2ezmFnkrWRhOme9y/8M1MIo2OKIRBIMgjOV/W63VDcUls"
+              integrity="sha384-DKYJZ8NLiK8MN4/C5P2dtSmLQ4KwPaoqAfyA/DfmEc1VDxu4yyC7wy6K1Hs90nka"
               crossOrigin="anonymous"
               strategy="afterInteractive"
             />
